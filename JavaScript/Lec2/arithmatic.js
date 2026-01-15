45b4512c50dde31291156249e191405d7ffcc0d5
Anuja Khandale <khandaleanuja3@gmail.com>
2026-01-15 14:13:55 +0530

    Lec 2

---
+++ JavaScript/Lec2/arithmatic.js
@@ -0,0 +1,32 @@
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+
+
+//Addition of string
+// let str1 = "Hello ";
+// let str2 = "Anuja";
+// console.log(str1 + str2);
+
+
+// JS only automatically converts a string to a number in arithmatic operation 
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "2")
+
+
+//Activity 2
+// console.log(true);
+// console.log(+ true);
+// console.log(+ "")
+
+
+// let getcounter = 100;
+// getcounter++
+// console.log(getcounter)
+
+let scorecounter = 200;
+scorecounter++;
+console.log(scorecounter);
+
---
+++ JavaScript/Lec2/conversion.js
@@ -0,0 +1,15 @@
+// Number to String
+let num = 33;
+let str = String(num);
+console.log(str)
+
+console.log( `Type of str : ${typeof(str)}`)
+
+// String to number
+let num1 = Number(str)
+console.log(num1)
+console.log( `Type of num1 : ${typeof(num1)}`)
+
+// Activity 1   ---> Do conversion of all data types
+
+
